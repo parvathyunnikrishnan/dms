28e8c6154135e2482871be5f64288ad5e1ae2c44
Lekshmi J <lekshmi.j@seqato.com>
2021-09-30 11:12:07 +0530

    modified files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+/*import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -15,4 +15,45 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+export class AppModule { }*/
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CareerComponent } from './careers/careers.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { AboutusComponent } from './aboutus/aboutus.component'
+
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'blogs', component: BlogsComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'careers', component: CareerComponent },
+  { path: 'home', component: HomeComponent }
+];
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CareerComponent,
+    BlogsComponent,
+    AboutusComponent
+  ],
+
+  bootstrap: [AppComponent]
+})
 export class AppModule { }
+
